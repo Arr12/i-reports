c16e71bfe10af4d8506fafddb1ce199a9785edcb
Arr12 <aryo.puruhito12@gmail.com>
2021-06-14 08:40:09 +0700

    non activated schedule running

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('daily:update')->dailyAt('01:00');
+        // $schedule->command('daily:update')->dailyAt('01:00');
     }
 
     /**
